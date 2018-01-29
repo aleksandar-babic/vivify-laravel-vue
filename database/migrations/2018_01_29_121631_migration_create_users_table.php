91e7b86d32e6b48e017cef6a29203a9ffc35c914
Aleksandar Babic <alekski64@icloud.com>
2018-01-29 13:21:11 +0100

    Added migration for users table creation

---
+++ database/migrations/2018_01_29_121631_migration_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MigrationCreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('company');
+            $table->string('country');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
