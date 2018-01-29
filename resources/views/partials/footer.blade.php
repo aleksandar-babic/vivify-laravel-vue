56af1649da621804e9871192b5f68a9253cf201f
Aleksandar Babic <alekski64@icloud.com>
2018-01-29 16:50:24 +0100

    Added header and footer partials

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <footer class="footer">
+        <!--Footer Links-->
+        <div class="container-fluid">
+            <div class="row">
+                <!--First column-->
+                <div class="col-md-6">
+                    <h5 class="title">Footer Content</h5>
+                </div>
+                <!--/.First column-->
+            </div>
+        </div>
+        <!--/.Footer Links-->
+    </footer>
+</header>
