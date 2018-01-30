c22fcf1c2ff914a53441d52e75a65d4dac8babe9
Aleksandar Babic <alekski64@icloud.com>
2018-01-30 14:14:42 +0100

    Implemented JWT authentication for Rest API

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,16 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'ApiAuthController@login');
+    Route::post('logout', 'ApiAuthController@logout');
+    Route::post('refresh', 'ApiAuthController@refresh');
+    Route::post('me', 'ApiAuthController@me');
+
 });
