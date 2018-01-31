87b5eb46dbd94ffec0fb62071a0efa76a7c05394
Aleksandar Babic <alekski64@icloud.com>
2018-01-31 13:13:27 +0100

    Minor changes to task factory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -3,11 +3,11 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Task::class, function (Faker $faker) {
-    $users = App\User::all()->pluck('id')->toArray();
+    $users = App\User::all()->pluck('id');
     return [
         'content' => $faker->text,
         'priority' => 'medium',
         'done'=> false,
-        'user_id' => $faker->randomElement($users)
+        'user_id' => $users->random()
     ];
 });
